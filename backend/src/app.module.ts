f7bf49a5f94df0407f96136ab60a7b1e8a1ff64f
York <31153913+Yorkchung@users.noreply.github.com>
2026-02-02 00:55:03 +0800

    feat(backend): update app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,15 +2,28 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CircleModule } from './circle/circle.module';
+import { circleConfig } from './config/circle.config';
+import { appConfig } from './config/app.config';
+import { CircleModule } from './modules/circle/circle.module';
+import { ChainModule } from './modules/chain/chain.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { PortfolioModule } from './modules/portfolio/portfolio.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+      load: [circleConfig, appConfig],
     }),
     CircleModule,
+    ChainModule,
+    UsersModule,
+    ProductsModule,
+    PaymentsModule,
+    PortfolioModule,
   ],
   controllers: [AppController],
   providers: [AppService],
