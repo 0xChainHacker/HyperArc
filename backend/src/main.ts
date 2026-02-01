6ff4157a8f67155de15efa25fa25fe5b365b0ed9
York <31153913+Yorkchung@users.noreply.github.com>
2026-02-02 00:22:07 +0800

    chore: update env port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3001);
+  const PORT = process.env.PORT || 3001;
+  await app.listen(PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
